d129754f1398edda4b33c6017223fa5ac99122ae
Zach Cali <cali@Zachs-MacBook-Air.local>
2022-06-15 16:31:02 +0800

    status update

--- client/src/components/Elements/DropDown.js
+++ client/src/components/Elements/DropDown.js
@@ -1,21 +1,33 @@
-import { useState, useEffect, useRef, useImperativeHandle, forwardRef } from "react";
+import {
+  useState,
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from "react";
 import { BsFilter as FiltersIcon } from "react-icons/bs";
 import Card from "./card";
 
-// @params { children, label, buttonClass, labelClass, iconClass, cardItemClass }
+// @params { children, label, buttonClass, labelClass, iconClass, cardItemClass, menu }
 const DropDown = forwardRef((props, ref) => {
   const innerref = useRef();
   const [open, setOpen] = useState(false);
+  const [buttonColor, setButtonColor] = useState("bg-white");
 
   useImperativeHandle(ref, () => ({
     toggle() {
       setOpen(!open);
     },
+    setButtonColor,
   }));
 
   useEffect(() => {
     const checkForOutsideClick = (event) => {
-      if (open && innerref.current && !innerref.current.contains(event.target)) {
+      if (
+        open &&
+        innerref.current &&
+        !innerref.current.contains(event.target)
+      ) {
         setOpen(false);
       }
     };
@@ -27,12 +39,19 @@ const DropDown = forwardRef((props, ref) => {
     };
   }, [open]);
 
+  useEffect(() => {
+    const buttonDetails = props.menu.filter((e) => {
+      return e.text === props.label.props.value;
+    });
+    setButtonColor(buttonDetails[0].color);
+  }, [buttonColor]);
+
   return (
     <div className="relative" ref={innerref}>
       <button
         className={`flex items-center justify-center gap-2 px-1 py-1 w-28 ${
           props.buttonClass ? props.buttonClass : ""
-        } rounded-lg`}
+        } ${buttonColor} rounded-lg`}
         onClick={() => setOpen(!open)}
         aria-label={`${open ? "Hide filters menu" : "Show filters menu"}`}
       >
@@ -52,7 +71,9 @@ const DropDown = forwardRef((props, ref) => {
       >
         <Card addClass="px-0 py-0">
           <div>
-            <div className={`flex flex-col gap-2 first:pt-2 last:pb-2 items-center ${props.cardItemClass}`}>
+            <div
+              className={`flex flex-col gap-2 first:pt-2 last:pb-2 items-center ${props.cardItemClass}`}
+            >
               {props.children}
             </div>
           </div>
--- client/src/pages/candidate.js
+++ client/src/pages/candidate.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import ContentNav from '../components/Elements/ContentNav';
-import Table from '../components/Tables/PrimaryTable';
-import Candidates from '../components/Tables/Candidates.json'
-import {CandidateCOLUMNS } from "../components/Tables/Columns"
+import React from "react";
+import ContentNav from "../components/Elements/ContentNav";
+import Table from "../components/Tables/PrimaryTable";
+import Candidates from "../components/Tables/Candidates.json";
+import { CandidateCOLUMNS } from "../components/Tables/Columns";
 
 function CandidateTab() {
-    return (
-        <>
-            <ContentNav>
-                <div className='Table'>
-                    <Table entryData={Candidates} tableColumns={CandidateCOLUMNS} newEntryLabel="Add Candidate" buttonUrl={"/app/add-candidate"}/>
-                </div>
-            </ContentNav>
-        </>
-    );
+  return (
+    <>
+      <ContentNav>
+        <div className="Table">
+          <Table
+            entryData={Candidates}
+            tableColumns={CandidateCOLUMNS}
+            newEntryLabel="Add Candidate"
+            buttonUrl={"/app/add-candidate"}
+            statusValue={[
+              { text: "Quick", color: "bg-[#5179DF]" },
+              { text: "Que", color: "bg-[#F47E7E]" },
+              { text: "Hired", color: "bg-[#5DA36D]" },
+            ]}
+            colIdxOfStatus={2}
+          />
+        </div>
+      </ContentNav>
+    </>
+  );
 }
 
 export default CandidateTab;
