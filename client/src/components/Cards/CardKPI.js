c3b50b7a71797ad2152f44ac00245b2b4f86e538
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-06-29 13:41:36 +0800

    spacing and naming conventions

--- client/src/components/Cards/CardKPI.js
+++ client/src/components/Cards/CardKPI.js
@@ -1,31 +1,57 @@
 import React from "react";
-import Card from "../Elements/card";
+import Card from "../Elements/Card";
 // import { FaChevronLeft as Icon } from "react-icons/fa";
-import { BsGraphUp as Trend, BsPlus as Plus } from "react-icons/bs"
+import {
+  BsGraphUp as GraphUp,
+  BsPlus as Plus,
+  BsGraphDown as GraphDown,
+} from "react-icons/bs";
+import { BiMinus as Minus } from "react-icons/bi";
 // import { BiDollar as Dollar } from "react-icons/bi"
-function CardKpi({ children, id, header, subHeader,subSpan, spanColor }) {
-    return (
-    <Card addClass="border-none w-64 h-32 rounded-[18px] CardKPI">
-        <div className="pb-1">
-            <div className="">
-                <div className="flex justify-between mb-1">
-                    <div className="text-lg font-bold font-Gothic text-almostBlack text-[#2B4993]">
-                    {header}
-                    </div>
-                    <span className={`text-lg ${spanColor}`}><Trend/></span>
-                </div>
-                <div className="flex justify-between">
-                    <div className="text-3xl font-bold text-almostBlack text-[#2B4993] flex items-center">
-                        {subHeader}
-                    </div>
-                    <span className={`text-lg ${spanColor} mt-[0.4rem] flex`}><Plus/>{subSpan}%</span>
-                </div>
-            </div>
-        </div>
+function CardKPI({
+  children,
+  header,
+  subHeader,
+  subSpan,
+  spanColor,
+  decline = false,
+}) {
 
-        {children}
+  return (
+    <Card addClass="border-none w-64 h-32 rounded-[18px] sm:CardKPI">
+      <div className="pb-1">
+        <div className="">
+          <div className="flex items-center justify-between mb-1">
+            <div className="text-lg font-bold font-Gothic text-almostBlack text-[#2B4993]">
+              {header}
+            </div>
+            <span
+              className={`text-lg ${
+                decline ? spanColor.decline : spanColor.incline
+              }`}
+            >
+              {decline ? <GraphDown /> : <GraphUp />}
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <div className="text-3xl font-bold text-almostBlack text-[#2B4993] flex items-center">
+              {subHeader}
+            </div>
+            <span
+              className={`text-lg flex items-center mt-[0.4rem] ${
+                decline ? spanColor.decline : spanColor.incline
+              }`}
+            >
+              {decline ? <Minus /> : <Plus />}
+              {subSpan}%
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {children}
     </Card>
-);
+  );
 }
 
-export default CardKpi;
+export default CardKPI;
