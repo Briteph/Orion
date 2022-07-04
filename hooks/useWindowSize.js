08e81ce87bb5fda791df79cc92b11c28c2b8c11e
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-07-04 15:13:13 +0800

    useWindowSize hook real-time update

--- hooks/useWindowSize.js
+++ hooks/useWindowSize.js
@@ -4,21 +4,12 @@ export default function useWindowSize() {
   const [windowDimensions, setWindowDimensions] = useState({});
 
   useEffect(() => {
-    window.addEventListener(
-      "resize",
+    window.addEventListener("resize", () => {
       setWindowDimensions({
         width: window.innerWidth,
         height: window.innerHeight,
-      })
-    );
-    return () =>
-      window.removeEventListener(
-        "resize",
-        setWindowDimensions({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        })
-      );
+      });
+    });
   }, []);
 
   return windowDimensions;
