18286243c0d5f9e7d1f4a639928d3d204fd718df
Code Monkey <56096375+codeMonkey20@users.noreply.github.com>
2022-07-07 15:20:10 +0800

    secondary button fix

--- components/Elements/ButtonSecondary.js
+++ components/Elements/ButtonSecondary.js
@@ -14,7 +14,7 @@ function ButtonSecondary({
       className={`px-3 py-1 rounded-lg font-bold text-[14px] border-2 shadow-sm ${
         disabled
           ? "bg-gray-200"
-          : "bg-[#FEAC00] border-[#FEAC00] hover:bg-yellow-300"
+          : "bg-[#FEAC00] border-yellow-300 hover:bg-yellow-300"
       } text-almostblack ${addClass}`}
       disabled={disabled}
       onClick={onClick}
